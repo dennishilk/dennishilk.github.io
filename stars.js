5a08bc3729c13fc65afcde8e72aff2e6ba2d3139
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-09 18:44:52 +0200

    Bust Peatland language cache

--- stars.js
+++ stars.js
@@ -1,6 +1,6 @@
 if (!document.querySelector('script[data-site-language-loader]')) {
   const languageScript = document.createElement('script');
-  languageScript.src = '/site-language.js?v=20260809-peatland-1';
+  languageScript.src = '/site-language.js?v=20260809-peatland-2';
   languageScript.dataset.siteLanguageLoader = 'true';
   document.head.appendChild(languageScript);
 }
